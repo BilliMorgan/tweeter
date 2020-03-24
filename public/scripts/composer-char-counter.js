38af5de9a60311b320e0295123ce390949d3d3f7
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-03-24 17:53:25 -0400

    style the page with css nad add counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('#tweet-text').on('keyup', function () {
+    const inputLength = $(this).val().length;
+    const maxInput = 140;
+    $('.counter').text(maxInput - inputLength)
+    if(maxInput-inputLength <= 0) {
+      $('.counter').css("color", "red")
+    }
+  })
+});
---
+++ public/scripts/tinker.js
@@ -0,0 +1,5 @@
+const logKey = (input) => {
+  input.toString()
+  console.log(140-input.length)
+};
+logKey("dfvdfvdfvdfvdfv");
